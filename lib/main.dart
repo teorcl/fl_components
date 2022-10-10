9eb44377c0350680473907910102f636bdb92390
Teodoro <teo.calle.lara@gmail.com>
2022-10-10 00:13:06 -0500

    Se agregan rutas de navegacion y barrel file o indice de exportaciones v1.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:fl_components/screens/listview2_screen.dart';
+import 'package:fl_components/screens/screens.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,7 +11,15 @@ class MyApp extends StatelessWidget {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: ListView2Screen()
+      //home: ListView2Screen(),
+      initialRoute: 'home',
+      routes: { 
+        'home'     :(BuildContext context) => const HomeScreen(),
+        'listview1':(BuildContext context) => ListView1Screen(),
+        'listview2':(BuildContext context) => ListView2Screen(), 
+        'alert'    :(BuildContext context) => const AlertScreen(),
+        'card'     :(BuildContext context) => const CardScreen(),     
+      },
     );
   }
 }
