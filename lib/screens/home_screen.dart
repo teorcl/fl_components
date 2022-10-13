08f42ca5aff267d9e1310f7c7a9495e77c6cdf36
Teodoro <teo.calle.lara@gmail.com>
2022-10-13 13:25:24 -0500

    Se implementa una navegacion basica desde cualquier elemento del HomeScreen hacia la pantalla de ListView1, esto implementando el metodo onTap del ListView.Separated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/screens/listview1_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,10 +7,22 @@ class HomeScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('HomeScreen'),
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('Componentes en Flutter'),
+        elevation: 0,
       ),
+      body: ListView.separated(
+        itemBuilder: (context, index) => ListTile(
+          leading: const Icon(Icons.access_alarms_sharp),
+          title: const Text('Nombre de ruta'),
+          onTap: () {
+            final route = MaterialPageRoute(builder: (context) => ListView1Screen(),);
+            Navigator.push(context, route);
+          },
+        ), 
+        separatorBuilder: (_, __) => const Divider(), 
+        itemCount: 10),
     );
   }
 }
