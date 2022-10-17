74a1373c1a663dc624c8f5c6cb8c1b2fff4ef7d2
Teodoro <teo.calle.lara@gmail.com>
2022-10-17 12:07:29 -0500

    Se agregan comentarios

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -15,21 +15,20 @@ class CardScreen extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         children: const [
           CustomCardType1(),
-          SizedBox(height: 10),
+          SizedBox(height: 20),
           CustomCardType2(
             nameCard: 'Paisaje 1',imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/3/35/Neckertal_20150527-6384.jpg'
           ),
-          SizedBox(height: 10),
+          SizedBox(height: 20),
           CustomCardType2(
             imageUrl: 'https://www.gardeningknowhow.com/wp-content/uploads/2020/12/lonely-japanese-cherry.jpg',
             nameCard: 'Arbol rosa',
           ),
-          SizedBox(height: 10),
+          SizedBox(height: 20),
           CustomCardType2(
             imageUrl: 'https://landscapetrades.com//assets/1652294870.007985.photo.00.1601578238_(plaque_photo).jpg',
-            nameCard: 'Paisaje nocturno',
           ),
-          SizedBox(height: 10),
+          SizedBox(height: 20),
           CustomCardType2(
             imageUrl: 'https://chiledescentralizado.cl/wp-content/uploads/2016/03/landscape-04.jpg',
           )
