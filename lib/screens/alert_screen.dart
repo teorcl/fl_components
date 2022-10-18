17d7d247bca648b34a57ce531de6fe962a9bd703
Teodoro <teo.calle.lara@gmail.com>
2022-10-18 01:28:54 -0500

    Se crea alerta de dialogo usando el paquete de cupertino design

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class AlertScreen extends StatelessWidget {
    
   const AlertScreen({Key? key}) : super(key: key);
+
+
+  void displayDialogIOS( BuildContext context){
+    showCupertinoDialog(
+      barrierDismissible: false,
+      context: context, 
+      builder: (context) {
+        return CupertinoAlertDialog(
+          title: const Text('Titulo'),
+
+          content: Column(
+            mainAxisSize: MainAxisSize.min ,
+            children: const [
+              Text('Este es el contenido de la alerta'),
+              SizedBox(height: 10),
+              FlutterLogo(size: 70,)
+            ],
+          ),
+
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context), 
+              child: const Text('Cancelar',style: TextStyle(color: Colors.red),)
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context), 
+              child: const Text('Ok')
+            )
+          ],
+        );
+      },
+    );
+  }
   
-  void displayDialog( BuildContext context){
+  void displayDialogAndroid( BuildContext context){
     showDialog(
       barrierDismissible: false, //Esto me permite cerrar el dialog cuando hago click en la parte oscura de la pantalla, si está en true
        context: context, 
@@ -27,7 +61,11 @@ class AlertScreen extends StatelessWidget {
           actions: [
             TextButton(
               onPressed: () => Navigator.pop(context), 
-              child: const Text('Cancelar')
+              child: const Text('Cancelar',style: TextStyle(color: Colors.red),)
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context), 
+              child: const Text('Ok')
             )
           ],
         );
@@ -42,7 +80,8 @@ class AlertScreen extends StatelessWidget {
       body: Center(
          child: ElevatedButton(
           // onPressed: () {},
-           onPressed: () => displayDialog(context),
+          //  onPressed: () => displayDialogAndroid(context),
+           onPressed: () => displayDialogIOS(context),
           //onPressed: displayDialog, 
           child: const Padding(
             padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
