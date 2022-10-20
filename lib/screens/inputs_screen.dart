25aa9e66b98c128f7d512120f55b2d4139276188
Teodoro <teo.calle.lara@gmail.com>
2022-10-20 01:46:10 -0500

    Se apersonaliza el TextFormField mediante la propiedad decoration y se le agrega validacion mediante la propiedad AutovalidationMode

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -17,12 +17,36 @@ class InputsScreen extends StatelessWidget {
             children: [
               TextFormField(
                 autofocus: true,
-                initialValue: 'Teodoro',
+                initialValue: '',
                 textCapitalization: TextCapitalization.words,
                 onChanged: (value) {
                   print('valie: $value');
                 },
-                
+                validator: (value) {
+                  if(value == null) return 'Este campo es requerido';
+                  return value.length < 3 ? 'Mínimo 3 letras' : null;
+                },
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: const InputDecoration(
+                  hintText: 'Nombre del usuario',
+                  labelText: 'Nombre',
+                  helperText: 'Solo caracteres',
+                  //prefixIcon: Icon(Icons.group_outlined),
+                  suffixIcon: Icon(Icons.group_outlined),
+                  icon: Icon(Icons.assignment_ind_outlined),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.green
+                    )
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                       
+                    )
+                  )
+                ),
               )
             ],
           ),
