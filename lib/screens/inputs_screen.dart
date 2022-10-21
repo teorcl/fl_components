e7e7bc43de4be260f33be98691a0ac189f2e10cf
Teodoro <teo.calle.lara@gmail.com>
2022-10-21 02:56:13 -0500

    Se centraliza el estilo de los inputs y se crea widget customInput

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/widgets.dart';
 
 class InputsScreen extends StatelessWidget {
    
@@ -14,40 +15,8 @@ class InputsScreen extends StatelessWidget {
          child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
           child: Column(
-            children: [
-              TextFormField(
-                autofocus: true,
-                initialValue: '',
-                textCapitalization: TextCapitalization.words,
-                onChanged: (value) {
-                  print('valie: $value');
-                },
-                validator: (value) {
-                  if(value == null) return 'Este campo es requerido';
-                  return value.length < 3 ? 'Mínimo 3 letras' : null;
-                },
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                decoration: const InputDecoration(
-                  hintText: 'Nombre del usuario',
-                  labelText: 'Nombre',
-                  helperText: 'Solo caracteres',
-                  //prefixIcon: Icon(Icons.group_outlined),
-                  suffixIcon: Icon(Icons.group_outlined),
-                  icon: Icon(Icons.assignment_ind_outlined),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.green
-                    )
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(10),
-                      topRight: Radius.circular(10),
-                       
-                    )
-                  )
-                ),
-              )
+            children: const [
+              CustomInputField(labelText: 'Nombre', hintText: 'Nombre de usuario',)
             ],
           ),
         ),
@@ -55,3 +24,4 @@ class InputsScreen extends StatelessWidget {
     );
   }
 }
+
