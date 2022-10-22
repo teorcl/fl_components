3e8cef5294cf3112e1566dc9324e967ebc671538
Teodoro <teo.calle.lara@gmail.com>
2022-10-22 01:56:13 -0500

    Video 91, se aplica el widget Form que me permite hacer validaciones en los campos y se da funcionalidad al boton de guardar

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -7,6 +7,17 @@ class InputsScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
+
+    final GlobalKey<FormState> myFormKey = GlobalKey<FormState>();
+
+    final Map<String, String> formValues = {
+      'first_name':'Teodoro',
+      'last_name' :'Calle',
+      'email'     :'teo.calle.lara@google.com',
+      'password'  :'123456',
+      'role'      :'administrador'
+    };
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Inputs y Forms'),
@@ -14,22 +25,44 @@ class InputsScreen extends StatelessWidget {
       body: SingleChildScrollView(
          child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-          child: Column(
-            children: const [
+          child: Form(
+            key: myFormKey, //Esto lo hago despues de definir el formkey
+            child: Column(
+              children:  [
+                
+                const CustomInputField(labelText: 'Nombre', hintText: 'Nombre del usuario'),
+                const SizedBox(height: 30),
+          
+                const CustomInputField(labelText: 'Apellido', hintText: 'Apellido del usuario'),
+                const SizedBox(height: 30),
+          
+                const CustomInputField(labelText: 'Email', hintText: 'Correo del usuario',keyboardType: TextInputType.emailAddress,),
+                const SizedBox(height: 30),
+          
+                const CustomInputField(labelText: 'Password', hintText: 'Contraseña',isObscureText: true,),
+                const SizedBox(height: 30),
+          
+                ElevatedButton(
+                  child: const SizedBox(
+                    width: double.infinity,
+                    height: 50,
+                    child: Center(child: Text('Guardar',style: TextStyle(fontSize:22),))
+                  ),
+                  onPressed: () {
+
+                    FocusScope.of(context).requestFocus(FocusNode());
+
+                    if(!myFormKey.currentState!.validate()){
+                      print('Formulario no válido');
+                      return;
+                    }
+                    // TODO: Cuando haga tap en el boton quiero imprimir los valores del formulario
+                    print(formValues);
+                  },
+                )
               
-              CustomInputField(labelText: 'Nombre', hintText: 'Nombre del usuario'),
-              SizedBox(height: 30),
-
-              CustomInputField(labelText: 'Apellido', hintText: 'Apellido del usuario'),
-              SizedBox(height: 30),
-
-              CustomInputField(labelText: 'Email', hintText: 'Correo del usuario',keyboardType: TextInputType.emailAddress,),
-              SizedBox(height: 30),
-
-              CustomInputField(labelText: 'Password', hintText: 'Contraseña',isObscureText: true,),
-              SizedBox(height: 30)
-            
-            ],
+              ],
+            ),
           ),
         ),
       ),
