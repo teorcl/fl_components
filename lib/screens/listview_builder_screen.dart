502278356be083ddb279e6e246b4892192e0f3ba
Teodoro <teo.calle.lara@gmail.com>
2022-10-23 02:15:01 -0500

    Video 96,7, se aplica el infinite scroll esto no es un widget sino un controlador que possen los widget con scroll

--- lib/screens/listview_builder_screen.dart
+++ lib/screens/listview_builder_screen.dart
@@ -1,9 +1,35 @@
 import 'package:flutter/material.dart';
 
-class ListViewBuilderScreen extends StatelessWidget {
+class ListViewBuilderScreen extends StatefulWidget {
    
   const ListViewBuilderScreen({Key? key}) : super(key: key);
-  
+
+  @override
+  State<ListViewBuilderScreen> createState() => _ListViewBuilderScreenState();
+}
+
+class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
+
+  final List<int> imagesIds = [1,2,3,4,5,6,7,8,9,10];
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController.addListener(() { 
+      //print('${scrollController.position.pixels}, ${scrollController.position.maxScrollExtent}');
+      if((scrollController.position.pixels + 500 ) >= scrollController.position.maxScrollExtent){
+        addFiveElement();
+      }
+    }) ; 
+  }
+
+  void addFiveElement(){
+    final lastId = imagesIds.last;
+    imagesIds.addAll([1,2,3,4,5].map((e) => lastId + e));//Iterable)
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,11 +38,13 @@ class ListViewBuilderScreen extends StatelessWidget {
         removeTop: true,
         removeBottom: true,
         child: ListView.builder(
-          itemCount: 10,
+          physics: const BouncingScrollPhysics(),
+          controller: scrollController,
+          itemCount: imagesIds.length,
           itemBuilder: (BuildContext context, int index) {
             return  FadeInImage(
               placeholder: const AssetImage('assets/jar-loading.gif'), 
-              image: NetworkImage('https://picsum.photos/500/300?image=${index+1}'),
+              image: NetworkImage('https://picsum.photos/500/300?image=${ imagesIds[index] }'),
               height: 230,
               width: double.infinity,
               fit: BoxFit.cover, 
